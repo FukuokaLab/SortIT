db3285e3868a335b41708ab63a9d053d96e54573
eokoshi <127910259+eokoshi@users.noreply.github.com>
2025-11-14 16:42:08 +0900

    updated base template to pass crsf token for logout (django 5.0 change)

--- account/views.py
+++ account/views.py
@@ -1,23 +1,25 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+)
 from django.views import generic
-from django.contrib.auth import get_user_model 
 from django.contrib.auth.mixins import UserPassesTestMixin
 from .forms import LoginForm, SignupForm, UserUpdateForm, MyPasswordChangeForm, User
 from django.shortcuts import redirect, resolve_url
 from django.urls import reverse_lazy
-from django.views.decorators.csrf import csrf_protect
 
 
-'''Add'''
 class Login(LoginView):
     form_class = LoginForm
-    template_name = 'account/login.html'
+    template_name = "account/login.html"
+
 
 class Logout(LogoutView):
-    template_name = 'account/logout_done.html'
+    template_name = "account/logout_done.html"
+
 
 # A mixin that allows access only to the user themselves.
 class OnlyYouMixin(UserPassesTestMixin):
@@ -25,21 +27,21 @@ class OnlyYouMixin(UserPassesTestMixin):
 
     def test_func(self):
         user = self.request.user
-        return user.pk == self.kwargs['pk']
+        return user.pk == self.kwargs["pk"]
 
 
 class MyPage(OnlyYouMixin, generic.DetailView):
     model = User
-    template_name = 'account/my_page.html'
+    template_name = "account/my_page.html"
 
 
 class Signup(generic.CreateView):
-    template_name = 'account/user_form.html'
-    form_class =SignupForm
+    template_name = "account/user_form.html"
+    form_class = SignupForm
 
     def form_valid(self, form):
-        user = form.save() 
-        return redirect('account:signup_done')
+        user = form.save()
+        return redirect("account:signup_done")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -48,15 +50,16 @@ class Signup(generic.CreateView):
 
 
 class SignupDone(generic.TemplateView):
-    template_name = 'account/sign_up_done.html'
+    template_name = "account/sign_up_done.html"
+
 
 class UserUpdate(OnlyYouMixin, generic.UpdateView):
     model = User
     form_class = UserUpdateForm
-    template_name = 'account/user_form.html'
+    template_name = "account/user_form.html"
 
     def get_success_url(self):
-        return resolve_url('account:my_page', pk=self.kwargs['pk'])
+        return resolve_url("account:my_page", pk=self.kwargs["pk"])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -66,13 +69,15 @@ class UserUpdate(OnlyYouMixin, generic.UpdateView):
 
 class PasswordChange(PasswordChangeView):
     form_class = MyPasswordChangeForm
-    success_url = reverse_lazy('account:password_change_done')
-    template_name = 'account/user_form.html'
+    success_url = reverse_lazy("account:password_change_done")
+    template_name = "account/user_form.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["process_name"] = "Change Password"
         return context
 
+
 class PasswordChangeDone(PasswordChangeDoneView):
-    template_name = 'account/password_change_done.html'
+    template_name = "account/password_change_done.html"
+
