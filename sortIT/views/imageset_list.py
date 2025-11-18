4c5d3dcf3b58682ca480e6a2ad9e8690a237d434
eokoshi <127910259+eokoshi@users.noreply.github.com>
2025-11-18 23:42:51 +0900

    feat(imageset_list): updated card appearance

--- sortIT/views/imageset_list.py
+++ sortIT/views/imageset_list.py
@@ -1,10 +1,11 @@
 import datetime
+
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseBadRequest, StreamingHttpResponse
 from django.shortcuts import redirect, render
 
-from sortIT.admin import generate_csv_stream
 from sortIT.models import ImageSet
+from sortIT.utils import generate_csv_stream
 
 
 @login_required
@@ -15,8 +16,7 @@ def choose_img_set(request, project_id):
         context = {"object_list": imagesets}
         return render(request, "sortIT/imageset_list.html", context)
 
-    else:
-        return redirect("/admin/sortIT/imageset/add/")
+    return redirect("/admin/sortIT/imageset/add/")
 
 
 @login_required
@@ -24,7 +24,7 @@ def download_csv(request, imageset_id):
     if request.method == "GET":
         current_datetime = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         imageset = ImageSet.objects.get(id=imageset_id)
-        csv_generator = generate_csv_stream(image_set=imageset)
+        csv_generator = generate_csv_stream(imageset)
         response = StreamingHttpResponse(csv_generator, content_type="text/csv")
         response["Content-Disposition"] = (
             f'attachment; filename="{imageset.name}_{imageset.project.name}_{current_datetime}.csv"'
